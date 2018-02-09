b43619d5fd34eb1fed80b3731cedd478fc8726a9
Devin Janus <devinjanus@D.Janus-MBP-8GB>
2018-02-08 21:32:45 -0500

    got response, now just need to fix oauth

--- src/index.js
+++ src/index.js
@@ -94,7 +94,7 @@ controller.hears(['hello'], 'direct_message,direct_mention', (bot, message) => {
   bot.reply(message, 'what it do fam')
 })
 
-controller.hears(['*.'], 'direct_message,direct_mention', (bot, message) => {
+controller.hears(['problem'], 'direct_message,direct_mention', (bot, message) => {
   console.log(`Message:\n${util.inspect(message)}`)
   
   // 1. parse relavent info from message body
