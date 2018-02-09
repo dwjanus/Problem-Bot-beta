03cd96db17c59b6b8f0fb9129df0fa0976eb5e22
Devin Janus <devinjanus@D.Janus-MBP-8GB>
2018-02-09 16:40:55 -0500

    oauth flow basically works now, testing problem info parse

--- src/index.js
+++ src/index.js
@@ -20,13 +20,14 @@ if (!config('SLACK_CLIENT_ID') || !config('SLACK_CLIENT_SECRET')) {
 
 const controller = Botkit.slackbot({
   interactive_replies: true,
+  rtm_receive_messages: false,
+  token: config('SLACK_BOT_TOKEN'),
   clientVerificationToken: config('SLACK_VERIFY'),
   storage: mongoStorage
 }).configureSlackApp({
   clientId: config('SLACK_CLIENT_ID'),
   clientSecret: config('SLACK_CLIENT_SECRET'),
   redirectUri: 'https://problem-bot-beta.herokuapp.com/oauth',
-  rtm_receive_messages: false,
   scopes: ['bot', 'incoming-webhook']
 })
 
@@ -204,6 +205,8 @@ controller.on('interactive_message_callback', (bot, trigger) => {
       'Submit',
       elements
     )
+
+    dialog.trigger_id = trigger.trigger_id,
 
     // const dialog = {
     //   trigger_id: trigger.trigger_id,
--- src/lib/config.js
+++ src/lib/config.js
@@ -12,6 +12,7 @@ const config = {
   SLACK_CLIENT_ID: process.env.SLACK_CLIENT_ID,
   SLACK_CLIENT_SECRET: process.env.SLACK_CLIENT_SECRET,
   SLACK_VERIFY: process.env.SLACK_VERIFY,
+  SLACK_BOT_TOKEN: process.env.SLACK_BOT_TOKEN,
   SF_ID: process.env.SF_ID,
   SF_SECRET: process.env.SF_SECRET,
   MONGODB_URI: process.env.MONGODB_URI
